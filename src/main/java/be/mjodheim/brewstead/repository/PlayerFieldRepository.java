05095f46896957e803ad53229b73e337ed4c9bf8
Anthony Mets <110968830+mjodheim@users.noreply.github.com>
2026-09-17 11:09:33 +0200

    Ajout de la méthode 'findAllFields' qui retourne toutes les parcelles d'un joueur.

--- src/main/java/be/mjodheim/brewstead/repository/PlayerFieldRepository.java
+++ src/main/java/be/mjodheim/brewstead/repository/PlayerFieldRepository.java
@@ -1,7 +1,13 @@
 package be.mjodheim.brewstead.repository;
 
 import be.mjodheim.brewstead.entity.PlayerField;
+import be.mjodheim.brewstead.entity.PlayerProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlayerFieldRepository extends JpaRepository<PlayerField, Long> {
+
+    // Toutes les parcelles appartenant à un joueur
+    List<PlayerField> findAllByPlayer (PlayerProfile player);
 }
--- src/main/java/be/mjodheim/brewstead/service/FarmService.java
+++ src/main/java/be/mjodheim/brewstead/service/FarmService.java
@@ -2,6 +2,7 @@ package be.mjodheim.brewstead.service;
 
 import be.mjodheim.brewstead.entity.Crop;
 import be.mjodheim.brewstead.entity.PlayerField;
+import be.mjodheim.brewstead.entity.PlayerProfile;
 import be.mjodheim.brewstead.enums.FieldStatus;
 import be.mjodheim.brewstead.repository.PlayerFieldRepository;
 import jakarta.transaction.Transactional;
@@ -9,14 +10,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FarmService {
 
     private final PlayerFieldRepository playerFieldRepository;
-
     private final InventoryService inventoryService;
+
+    public List<PlayerField> findAllFields(PlayerProfile playerProfile) {
+        return playerFieldRepository.findAllByPlayer(playerProfile);
+    }
 
     @Transactional
     public PlayerField plant(PlayerField field, Crop crop) {
@@ -51,11 +56,37 @@ public class FarmService {
         if(
             FieldStatus.GROWING.equals(field.getStatus())
                 &&
-            LocalDateTime.now().isAfter(field.getPlantedAt())
+            LocalDateTime.now().isAfter(field.getReadyAt())
         ) {
             // Mise à jour de l'état de la parcelle
             field.setStatus(FieldStatus.READY);
         }
+
+        return playerFieldRepository.save(field);
+    }
+
+    @Transactional
+    public PlayerField harvest (PlayerField field) {
+
+        // On vérifie si la parcelle est prête à être récoltée
+        if(!field.getStatus().equals(FieldStatus.READY)) {
+            throw  new IllegalStateException("Field is not ready");
+        }
+
+        // Ajouter la culture dans l'inventaire du joueur
+        inventoryService.addIngredient(
+                field.getPlayer(),
+                field.getCrop().getIngredient(),
+                field.getCrop().getYieldQuantity()
+        );
+
+        // Mise à jour de l'état de la parcelle
+        field.setStatus(FieldStatus.EMPTY);
+
+        // Remise à zéro de ses valeurs
+        field.setCrop(null);
+        field.setPlantedAt(null);
+        field.setReadyAt(null);
 
         return playerFieldRepository.save(field);
     }

7fd1692dc3ed7035121eaa5681cdbdc803eed967
Anthony Mets <110968830+mjodheim@users.noreply.github.com>
2026-09-17 18:42:25 +0200

    feat(domain): prepare remaining gameplay services

--- src/main/java/be/mjodheim/brewstead/repository/BeehiveRepository.java
+++ src/main/java/be/mjodheim/brewstead/repository/BeehiveRepository.java
@@ -3,5 +3,9 @@ package be.mjodheim.brewstead.repository;
 import be.mjodheim.brewstead.entity.Beehive;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BeehiveRepository extends JpaRepository<Beehive, Long> {
+
+    List<Beehive> findAllByPlayerId(Long playerId);
 }
--- src/main/java/be/mjodheim/brewstead/repository/NpcOrderLineRepository.java
+++ src/main/java/be/mjodheim/brewstead/repository/NpcOrderLineRepository.java
@@ -3,5 +3,9 @@ package be.mjodheim.brewstead.repository;
 import be.mjodheim.brewstead.entity.NpcOrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NpcOrderLineRepository extends JpaRepository<NpcOrderLine, Long> {
+
+    List<NpcOrderLine> findAllByOrderId(Long orderId);
 }
